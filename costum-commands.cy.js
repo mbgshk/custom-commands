363e94f40bcf800ef7d411a75505f1b313d01bb2
mbgshk <mbgshk@gmail.com>
2022-07-09 08:19:55 +0700

    Add Implementation

--- costum-commands.cy.js
+++ costum-commands.cy.js
@@ -1,1 +1,9 @@
 /// <reference types="cypress" />
+
+// Implementation
+describe('Custom commands', () => {
+    it('Should load login page', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html')
+        cy.login('username', 'password')
+    });
+})
