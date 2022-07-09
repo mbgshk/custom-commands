b8eb543fb1f3eb9f15ab19d9f3472aebea43f3f2
mbgshk <mbgshk@gmail.com>
2022-07-09 09:10:49 +0700

    Submit Tugas

--- costum-commands.cy.js
+++ costum-commands.cy.js
@@ -1,9 +1,19 @@
 /// <reference types="cypress" />
 
 // Implementation
+// describe('Custom commands', () => {
+//     it('Should load login page', () => {
+//         cy.visit('http://zero.webappsecurity.com/login.html')
+//         cy.login('username', 'password')
+//     });
+// })
+
+//Tugas
 describe('Custom commands', () => {
-    it('Should load login page', () => {
+    it('Should load pay-bills page', () => {
         cy.visit('http://zero.webappsecurity.com/login.html')
         cy.login('username', 'password')
+        cy.get('a').contains('Pay Bills').click()
+        cy.saving('payee', 'account', 'amount', 'date', 'desc')
     });
 })
